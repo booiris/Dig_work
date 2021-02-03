eb266f7d2a118f544b85a890820cb54370282f0f
booiris <954520770@qq.com>
2021-02-03 22:50:25 +0800

    update

--- work2/code_file/transform.cpp
+++ work2/code_file/transform.cpp
@@ -1,7 +1,18 @@
 #include <opencv.hpp>
+#include <cstdio>
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
 using namespace cv;
 
 int main()
 {
     Mat pic = imread("../data.jpg");
+    Rect rect(0, (int)(0.3 * pic.rows), pic.cols, (int)(0.7 * pic.rows));
+    Mat aim = pic(rect);
+
+    imshow("asd", aim);
+    waitKey(0);
+    
 }
